4303a7411851048151cc887fbb19b68969901352
Miguel Gil Alvarez <miguel.gila@atos.net>
2017-02-03 13:42:07 +0000

    Gradle Basic Config Floating Button Delete

--- app/src/main/java/com/example/popularmovies/activity/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/activity/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.popularmovies.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -18,24 +16,15 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    //MENÚ
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -46,6 +35,8 @@ public class MainActivity extends BaseActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+             //       .setAction("Action", null).show();
         }
 
         return super.onOptionsItemSelected(item);
